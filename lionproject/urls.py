a51cfc511ee9e605a5737e8c892097f50ccb5192
Young-ho Jung <youngh0910@gmail.com>
2020-07-25 17:31:10 +0900

    modify home/views.py and urls.py

--- lionproject/urls.py
+++ lionproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('home/', views.index, name = 'home'),
+
 ]
