6190e05fd5e28bba7d71e7b1b5d00c5449a0a7f9
yangahh <toaur6802@naver.com>
2020-07-30 05:44:23 +0900

    portfolio 추가

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from accounts.models import User_extend
+from requests.models import Request
+from estimates.models import Estimate
+from .models import Portfolio
 
-# Create your views here.
+
+def portfolioList(request):
+    
+    user_extend = User_extend.objects.get(user=request.user)  
+    context = {'user_extend' : user_extend,}
+    return render(request, 'portfolio/portfolio.html', context)
