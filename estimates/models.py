534ca74479150bff2295d9fe79af4863fb364c38
yangahh <toaur6802@naver.com>
2020-07-29 21:22:54 +0900

    estimate CRUD

--- estimates/models.py
+++ estimates/models.py
@@ -8,10 +8,10 @@ def set_none():
 
 class Estimate(models.Model):
     ptr_username = models.ForeignKey(User, null=False, on_delete=models.SET(set_none))
-    req_id = models.ForeignKey(Request, null=False, on_delete=models.SET(set_none))
+    req_id = models.ForeignKey(Request, null=False, on_delete=models.SET(set_none), related_name='related_request')
     price = models.IntegerField()
     title = models.CharField(max_length=100)
-    cont = models.TextField()
+    cont = models.TextField(null=False, blank=True)
     del_yn = models.CharField(max_length=1, default='N', null=False)
     reg_dtti = models.DateTimeField(auto_now_add=True)
     mod_dtti = models.DateTimeField(auto_now=True)
--- estimates/urls.py
+++ estimates/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.estimatesList, name='list'),
     path('<int:estimate_id>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
+    path('<int:request_id>/create', views.create, name='create'),
     path('<int:estimate_id>/edit/', views.edit, name='edit'),
     path('<int:estimate_id>/delete/', views.delete, name='delete'),
 ]
