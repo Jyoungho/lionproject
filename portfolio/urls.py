e5f31136f650b2bc4e308f483036e3702e680508
yangahh <toaur6802@naver.com>
2020-08-01 07:08:46 +0900

    포트폴리오 detail 기능 구현 ing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name= 'portfolio'
 urlpatterns = [
-    path('', views.portfolioList, name='list'),
+    path('', views.portfolioList_forP, name='list_forP'), # 파트너로 로그인 시
+    path('<int:estimate_id>/', views.portfolioList_forR, name='list_forR'), # 요청자로 로그인 시
+    path('detail/<int:portfolio_id>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('<int:portfolio_id>/edit/', views.edit, name='edit'),
+    path('<int:portfolio_id>/delete/', views.delete, name='delete'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,11 +4,102 @@ from django.contrib.auth.models import User
 from accounts.models import User_extend
 from requests.models import Request
 from estimates.models import Estimate
-from .models import Portfolio
+from .models import Portfolio, Image
 
+@login_required
+def portfolioList_forR(request, estimate_id): # 파트너 클릭 시 파트너 정보 넘어옴(User ,User_extend)
+    main_images = Image.objects.none()
+    # 현재 로그인 한 유저 정보 가져오기 (user_type이 필요)
+    user_extend = User_extend.objects.get(user=request.user)
 
-def portfolioList(request):
+    # 내 요청에 달린 견적서를 작성한 파트너 정보 가져오기
+    estimate = Estimate.objects.get(id=estimate_id) 
+    ptr_info = User_extend.objects.get(user=estimate.ptr_username)
+    # 그 파트너의 포트폴리오 리스트 가져오기
+    portfolios = Portfolio.objects.filter(ptr_username=ptr_info.user)
+    for portfolio in portfolios:
+        main_image = Image.objects.filter(portfolio_id=portfolio.id).filter(main_yn='Y')
+        main_images = main_images.union(main_image)
     
-    user_extend = User_extend.objects.get(user=request.user)  
-    context = {'user_extend' : user_extend,}
+    context = {
+        'user_extend' : user_extend,
+        'ptr_info' : ptr_info,
+        'portfolios' : portfolios,
+        'main_images' : main_images,
+    }
     return render(request, 'portfolio/portfolio.html', context)
+
+
+def portfolioList_forP(request):
+    main_images = Image.objects.none()
+    # 현재 로그인 한 유저 정보 가져오기 (user_type이 필요)
+    user_extend = User_extend.objects.get(user=request.user)
+    # 내 포트폴리오만 불러오기
+    portfolios = Portfolio.objects.filter(ptr_username=request.user)
+    for portfolio in portfolios:
+        main_image = Image.objects.filter(portfolio_id=portfolio.id).filter(main_yn='Y')
+        main_images = main_images.union(main_image)
+    
+    for test in main_images:
+        print(type(test))
+    
+    context = {
+        'user_extend' : user_extend,
+        'portfolios' : portfolios,
+        'main_images' : main_images,
+    }
+    return render(request, 'portfolio/portfolio.html', context)
+
+
+def detail(request, portfolio_id):
+    user_extend = User_extend.objects.get(user=request.user)
+    portfolio = Portfolio.objects.get(id=portfolio_id)
+    context = {
+        'user_extend' : user_extend,
+        'portfolio' : portfolio,
+    }
+    return render(request, 'portfolio/detail.html', context)
+
+
+@login_required
+def new(request):
+   
+    context = {
+        
+    }
+    # POST 방식일때
+    if request.method == 'POST':
+        return redirect('portfolio:list_forP')
+    # GET 방식일때    
+    else:
+        return render(request, 'portfolio/new.html', context)
+
+
+@login_required
+def edit(request, portfolio_id):
+    try:
+        portfolio = Portfolio.objects.get(id=estimate_id, ptr_username=request.user)
+    except Portfolio.DoesNotExist:
+        return redirect('home:home')
+
+    # POST 방식일때
+    if request.method == 'POST':
+
+
+
+        return redirect('portfolio:detail', portfolio_id=portfolio.id)
+    # GET 방식일때    
+    else:
+        context = {'portfolio' : portfolio}
+        return render(request, 'portfolio:edit.html', context)
+    
+
+
+@login_required
+def delete(request, portfolio_id):
+    try:
+        portfolio = Portfolio.objects.get(id=estimate_id, ptr_username=request.user)
+    except Portfolio.DoesNotExist:
+        return redirect('home:home')
+    portfolio.delete()
+    return redirect('portfolio:list_forP')
