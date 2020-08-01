e98c38d628fcb1770d1fec508d57c65613b6abdd
Young-ho Jung <youngh0910@gmail.com>
2020-08-01 19:14:18 +0900

    requests의 첫 페이지인 requests.html 기반구조만 작성

--- requests/urls.py
+++ requests/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name='requests'
 urlpatterns = [
     path('', views.requests, name = 'requests'),
-
+    path('new/', views.new, name = 'new'),
+    path('<int:requests_id>/', views.detail, name = 'detail'),
+    path('<int:requests_id>/', views.edit, name = 'edit'),
+    path('<int:requests_id>/', views.delete, name = 'delete'),
 ]
--- requests/views.py
+++ requests/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
 # Create your views here.
 
 def requests(request):
     context = {}
     return render(request, 'requests/requests.html', context)
+
+def new(request):
+    context = {}
+    return render(request, 'requests/new.html', context)
+
+def create(request):
+    context = {}
+    return render(request, 'requests/requests.html', context)
+
+def detail(request):
+    context = {}
+    return render(request, 'requests/detail.html', context)
+
+def edit(request):
+    context = {}
+    return render(request, 'requests/edit.html', context)
+
+def delete(request):
+    context = {}
+    return redirect('requests:requests')
