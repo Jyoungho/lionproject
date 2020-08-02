c86082d5cbbad9b0841121f3125fecea6df61eec
sopalas <shinji03264@gmail.com>
2020-08-03 04:08:27 +0900

    수정된 것: 1. 옵션 하드코딩 2. 마이페이지 사용자/파트너 나누기 3. 패스워드변경 4. 사용자 정보 변경 5. 마이페이지 프로필 사진 넣는 칸 만들기
    수정예정:
    1. 로그인 exception 처리 -> 어우...
    2. 사용자->파트너전환
    3. base.html 마이페이지들에서 나오게 만들기
    4. 프로필->사진 관련 파일만 받게 처리
    5. 활동지역 api -> 지도나 지역 불러오기

--- accounts/views.py
+++ accounts/views.py
@@ -22,15 +22,18 @@ def signup_partner(request):
                 username=request.POST['username'],
                 password=request.POST['password'],
             )
+            prof_img = None
+            if 'prof_img' in request.FILES:
+                prof_img = request.FILES['prof_img']
             nickname=request.POST['nickname'] #닉네임
             tel=request.POST['tel'] #전화번호
             actv_area=request.POST['actv_area'] #활동지역
             svc_cd=request.POST['svc_cd'] #서비스업종
             work_spc_kind=request.POST['work_spc_kind'] #작업가능공간종류
-            user_type='P'
-            ptr_join_yn='Y'
+            user_type='P' #파트너로 타입 지정
+            ptr_join_yn='Y' #파트너로 가입한 이력 yes로 만들어줌
 
-            new_user = User_extend(user=user, nickname=nickname, tel=tel, actv_area=actv_area, svc_cd=svc_cd, work_spc_kind=work_spc_kind, user_type=user_type, ptr_join_yn=ptr_join_yn)
+            new_user = User_extend(user=user, nickname=nickname, prof_img=prof_img, tel=tel, actv_area=actv_area, svc_cd=svc_cd, work_spc_kind=work_spc_kind, user_type=user_type, ptr_join_yn=ptr_join_yn)
             new_user.save() #세이브까지!
 
             auth.login(request, user)
@@ -39,7 +42,7 @@ def signup_partner(request):
             context['error'] = '정보를 확인해주세요.'
 
     #GET Method
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/partner.html', context)
 
 def signup_requester(request):
     
@@ -55,20 +58,19 @@ def signup_requester(request):
                 username=request.POST['username'],
                 password=request.POST['password'],
             )
+            prof_img = None
+            if 'prof_img' in request.FILES:
+                prof_img = request.FILES['prof_img']
             nickname=request.POST['nickname'] #닉네임
             tel=request.POST['tel'] #전화번호
-            print('##################전화번호까지')
             actv_area='N' #활동지역
             svc_cd='N' #서비스업종
             work_spc_kind='N'
             user_type='R'
             ptr_join_yn='N'
-            print("#####################데이터입력")
 
-            new_user = User_extend(user=user, nickname=nickname, tel=tel, actv_area=actv_area, svc_cd=svc_cd, work_spc_kind=work_spc_kind, user_type=user_type, ptr_join_yn=ptr_join_yn)
-            print('####################DATA 저장')
+            new_user = User_extend(user=user, nickname=nickname, prof_img=prof_img, tel=tel, actv_area=actv_area, svc_cd=svc_cd, work_spc_kind=work_spc_kind, user_type=user_type, ptr_join_yn=ptr_join_yn)
             new_user.save() #세이브까지!
-            print('####################SAVE')
 
             auth.login(request, user)
             return redirect('home:home')
@@ -76,7 +78,7 @@ def signup_requester(request):
             context['error'] = '정보를 확인해주세요.'
 
     #GET Method
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/requester.html', context)
 
 def login(request):
     
@@ -101,8 +103,8 @@ def login(request):
             context['error'] = "로그인 정보를 확인해주세요."
 
         # 아래 두 줄 꼭 기입하고 return시 context로 전달하기
-        user_extend = User_extend.objects.get(user=request.user)  
-        context = {'user_extend' : user_extend,}
+        #user_extend = User_extend.objects.get(user=request.user)  
+        #context = {'user_extend' : user_extend,}
     return render(request, 'accounts/login.html', context)
 
 def logout(request):
@@ -117,11 +119,11 @@ def exchange(request):
         if user_extend.ptr_join_yn == 'Y':
             user_extend.user_type = 'P'
             user_extend.save()
-        else:
+        else: #only 사용자로만 가입한 사람
             context = {}
             context['error'] = '파트너로 가입해주세요'
-            return render(request, 'accounts/signup.html', context)
-    else:
+            return render(request, 'accounts/partner.html', context)
+    else: #파트너로 로그인해서 고객으로 전환 클릭 시,
         user_extend.user_type = 'R'
         user_extend.save()
     return redirect('home:home')
--- mypage/views.py
+++ mypage/views.py
@@ -1,11 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
 from accounts.models import User_extend
-from .models import Mypage
+from django.contrib import auth
+#from .models import Mypage
 
 # Create your views here.
-
-def detail(request, user_id):
+def index(request):
     user_extend = User_extend.objects.get(user=request.user)
     context = {'user_extend': user_extend}
-    return render(request, 'mypage/detail.html', context)
+    return render(request, 'mypage/index.html', context)
+
+def change_pw(request):
+    context= {}
+
+    if request.method == "POST":
+        current_password = request.POST.get("origin_password")
+        user = request.user
+        if check_password(current_password,user.password):
+            new_password = request.POST.get("password1")
+            password_confirm = request.POST.get("password2")
+            if new_password == password_confirm:
+                user.set_password(new_password)
+                user.save()
+                auth.login(request,user)
+                return redirect("home:home")
+            else:
+                context.update({'error':"새로운 비밀번호를 다시 확인해주세요."})
+        else:
+            context.update({'error':"현재 비밀번호가 일치하지 않습니다."})
+
+    return render(request, "mypage/change_pw.html",context)
+
+def edit(request):
+    user_extend = User_extend.objects.get(user=request.user)
+    context = {'user_extend': user_extend}
+    return render(request, 'mypage/edit.html', context)
+
+def update(request):
+    user_extend = User_extend.objects.get(user=request.user)
+    user_extend.nickname=request.POST['nickname']
+    user_extend.tel=request.POST['tel']
+    if 'prof_img' in request.FILES:
+        user_extend.prof_img = request.FILES['prof_img']
+    if user_extend.user_type == 'P':
+        user_extend.actv_area=request.POST['actv_area']
+        user_extend.svc_cd=request.POST['svc_cd']
+        user_extend.work_spc_kind=request.POST['work_spc_kind']
+    user_extend.save()
+    return redirect('mypage:index')
