f8ec8efefafa2f688ef56760842955c54c7b25e3
yangahh <toaur6802@naver.com>
2020-08-07 22:15:47 +0900

    파트너 둘러보기 추가

--- home/views.py
+++ home/views.py
@@ -10,3 +10,16 @@ def index(request):
         user_extend = User_extend.objects.get(user=request.user)
         context = {'user_extend' : user_extend}
     return render(request, 'home/index.html', context)
+
+def partners(request):
+    partners = User_extend.objects.filter(user_type="P")
+
+    if not request.user.is_authenticated:
+        context = { 'partners' : partners }
+    else:
+        user_extend = User_extend.objects.get(user=request.user)
+        context = {
+            'user_extend' : user_extend,
+            'partners' : partners
+        }
+    return render(request, 'home/partners.html', context)
